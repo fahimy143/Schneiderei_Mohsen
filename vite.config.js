4f54196d65a5e68648ce106115c0e30ad0d8d255
Mustafa Fahimy <57341400+fahimy143@users.noreply.github.com>
2025-08-01 14:20:24 +0200

    bug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Schneiderei_Mohsen", // <- this must match your GitHub repo name
-});
+  base: '/Schneiderei_Mohsen/', // 👈 this MUST match your repo name exactly
+})
