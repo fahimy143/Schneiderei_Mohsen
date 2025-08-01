fd547f5fcdf52b6a4ade66796a26cc8dff081a13
Mustafa Fahimy <57341400+fahimy143@users.noreply.github.com>
2025-08-01 10:37:35 +0200

    initial setup

---
+++ tailoring-site/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-3xl font-bold underline">
+        Tailoring Website Coming Soon!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
