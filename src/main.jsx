e5b5a5bb6f1a16626f0ad5b5ca0fb5dcf39f652c
Mustafa Fahimy <57341400+fahimy143@users.noreply.github.com>
2025-08-01 15:23:30 +0200

    pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const App = () => (
+  <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+    <h1>👔 Schneiderei Mohsen</h1>
+    <p>
+      Welcome to our tailoring service. Excellence stitched into every seam.
+    </p>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
